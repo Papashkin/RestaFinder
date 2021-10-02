6c07bdd59aadbc20c70d0580745e661ee977bfa0
Papashkin <pavel.antoshkin@mooncascade.com>
2021-10-02 21:20:38 +0300

    Initial commit

---
+++ app/src/main/java/com/antsfamily/restafinder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.antsfamily.restafinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
